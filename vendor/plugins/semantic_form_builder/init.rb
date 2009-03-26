8ae8a78853c5719947cb8f117e84b27c47dea2e9
Geoffrey Dagley <gdagley@gmail.com>
2009-03-25 23:05:38 -0500

    removing some of the annoying stuff i thought i liked, but don't

--- vendor/plugins/semantic_form_builder/init.rb
+++
@@ -1,4 +0,0 @@
-require 'semantic_fields_renderer'
-require 'semantic_form_builder'
-require 'semantic_form_helpers'
-ActionView::Base.send :include, SemanticFormBuilder::Helpers
--- vendor/plugins/semantic_form_builder/tasks/semantic_form_builder_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :semantic_form_builder do
-#   # Task goes here
-# end
