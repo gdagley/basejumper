f03ff0680db92ef34d0fea597e53b02cf969caf3
Geoffrey Dagley <gdagley@gmail.com>
2009-09-18 23:35:56 -0500

    slight tidying for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'homepage'
 
-  map.login '/login', :controller => 'user_sessions', :action => 'new'
-  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.registration '/registration', :controller => 'users', :action => 'create'
-  map.admin '/admin', :controller => 'admin'
+  map.login        '/login',        :controller => 'user_sessions', :action => 'new'
+  map.logout       '/logout',       :controller => 'user_sessions', :action => 'destroy'
+  map.signup       '/signup',       :controller => 'users',         :action => 'new'
+  map.registration '/registration', :controller => 'users',         :action => 'create'
   
-  map.resource :account, :controller => 'users'
-  map.resource :password_reset
+  map.resource  :user_session
+  map.resource  :password_reset
+  map.resource  :account, :controller => 'users'
+  map.resource  :contact
   map.resources :users
-  map.resource :user_session
-  map.resource :contact
   
+  map.admin '/admin', :controller => 'admin'
   map.namespace(:admin) do |admin|
     admin.with_options(:active_scaffold => true) do |admin_scaffold|
       admin_scaffold.resources :users
