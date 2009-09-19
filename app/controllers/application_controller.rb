0de30ac14fe3cfe327166540438880ed2a6fbcc4
Geoffrey Dagley <gdagley@gmail.com>
2009-09-18 23:00:16 -0500

    simplifying wear things live

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  include AuthlogicSystem # see lib/authlogic_system
   include ExceptionNotifiable
   
   helper :all
@@ -10,6 +9,47 @@ class ApplicationController < ActionController::Base
   filter_parameter_logging :password, :password_confirmation
 
   protected
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+  
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.record
+    end
+    
+    def require_user
+      unless current_user && authorized?
+        store_location
+        flash[:notice] = "You do not have access to this page"
+        redirect_to login_url
+        return false
+      end
+    end
+    
+    def require_no_user
+      if current_user
+        store_location
+        flash[:notice] = "You must be logged out to access this page"
+        redirect_to account_url
+        return false
+      end
+    end
+  
+    def store_location
+      session[:return_to] = request.request_uri
+    end
+  
+    def redirect_back_or_default(default)
+      redirect_to(session[:return_to] || default)
+      session[:return_to] = nil
+    end
+    
+    def authorized?
+      true
+    end
+    
     def render_500
       respond_to do |type|
         type.html { render :template => "static/500", :status => "500 Error" }
