0b17f9f85582d97237977d808aa9658731e89c86
Geoffrey Dagley <gdagley@gmail.com>
2012-05-14 22:54:16 -0500

    get token auth syntax right

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, token_authenticatable,
+  devise :database_authenticatable, :token_authenticatable,
           :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
