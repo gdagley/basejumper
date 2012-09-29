3da7ead88bc14e1166682268c116952df70ca7e2
Geoffrey Dagley <gdagley@gmail.com>
2012-09-29 07:17:52 -0500

    only use generators i like

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,17 @@ module BaseJumper
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
 
